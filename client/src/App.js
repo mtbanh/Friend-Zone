9f01a533d6282f027f8685215955c7dd55c4dbe0
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-18 11:08:32 -0700

    added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./Components/Navbar"
 import Chat from "./pages/Chat";
 import Landing from "./pages/Landing";
 import NoMatch from "./pages/NoMatch";
@@ -13,11 +14,13 @@ import Profile from "./pages/Profile";
 function App() {
   return (
     <Router>
+      <div>
+        <Nav />
         <Switch>
-          {/* <Route exact path={["/", "/landing"]}>
+          <Route exact path={["/", "/landing"]}>
             <Landing />
           </Route>
-          <Route exact path="/signin">
+          {/* <Route exact path="/signin">
             <Signin />
           </Route>
           <Route exact path="/signup">
@@ -36,6 +39,7 @@ function App() {
             <NoMatch />
           </Route>
         </Switch>
+        </div>
     </Router>
   );
 }
