ec307e5fbdc30610ac2f98be730e7cd1c7a7374c
Aidan Sweeny <apswee22@colby.edu>
2020-08-19 16:15:31 -0400

    Trying to add scrolling feature

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -9,7 +9,6 @@ const Chat = () => {
     const [chatId, setChatId] = useState(0)
     const [showChat, setShowChat] = useState(false)
     const scroller = createRef();
-
     useEffect(() => {
         updateScroll()
         if (!loadChats()) { console.log("Nothing to see here") }
@@ -58,7 +57,8 @@ const Chat = () => {
                 var newTran = transactions
                 newTran.push(res.data)
                 setTransactions(newTran)
-                window.location.reload(false);
+                console.log(transactions)
+                // window.location.reload(false);
             })
     }
 
@@ -82,6 +82,28 @@ const Chat = () => {
         return string;
     }
 
+    function update(){
+        setInterval(() => {
+            transactions.map(tran => {
+            {
+                if (tran.userID == userName) {
+                    return (
+                        <div key={tran.id} className="card">
+                            <div className='bg-info card-body' key={tran.id}>{JSON.parse(formatDate(tran.timeStamp))}<div style={{ witdth: "100%", height: "1px", backgroundColor: "black" }}></div>{tran.text}</div>
+                        </div>
+                    )
+                }
+                else {
+                    return (
+                        <div key={tran.id} className="card">
+                            <div className='bg-secondary card-body' key={tran.id}>{JSON.parse(formatDate(tran.timeStamp))}<div style={{ witdth: "100%", height: "1px", backgroundColor: "black" }}></div>{tran.text}</div>
+                        </div>
+                    )
+                }
+            }
+
+        })}, 1000)
+    }
     return (
         <div className="container">
             <button onClick={handleClick}>New Chat</button>
@@ -95,7 +117,7 @@ const Chat = () => {
                 </ul>
                     <div className  = "col-md-8">
                     <h3 style={{ display: showChat ? "block" : "none" }} >Chat with: {friendName}</h3>
-                    <div ref={scroller}  style={{ height: "300px", overflow: "scroll" }}>
+                    <div ref={scroller}  onLoad = {updateScroll} style={{ height: "300px", overflow: "scroll" }}>
                         {transactions.map(tran => {
                             {
                                 if (tran.userID == userName) {
@@ -115,6 +137,7 @@ const Chat = () => {
                             }
 
                         })}
+                        {update()}
                         <div ref={scroller} ></div>
                     </div>
                     <div className = "row">
