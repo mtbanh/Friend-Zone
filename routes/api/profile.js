13a1a6c70b1d9ce8557c45f8d0dfd5138c22a448
Isaias Del Mundo <idelmundo68@gmail.com>
2020-08-25 11:06:55 -0700

    routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,15 +1,4 @@
 const router = require("express").Router();
-<<<<<<< HEAD
-const profile = require("../../controllers/profileController");
-
-router.route("/")
-    .post( profile.createProfile)
-
-
-   
-
-module.exports = router
-=======
 const profileController = require("../../controllers/profileController");
 // Matches with "/api/chat"
 router
@@ -21,4 +10,3 @@ router
   .route("/:id")
   .get(profileController.getById)
 module.exports = router;
->>>>>>> 51ef9d710e19053c5835f5952b4d5bf087cebf98
