d8b3316ea50c4925f1077c0a85b29162b4ac6288
jdavis3333 <jdavis3333@gmail.com>
2020-08-29 11:06:03 -0700

    updated seed file

--- client/src/pages/AddFriends.js
+++ client/src/pages/AddFriends.js
@@ -88,7 +88,9 @@ const Addfriends = () => {
 
             <Cards
               name={friend.firstName + " " +friend.lastName}
+              age={friend.age}
               hobby={friend.hobby}
+              bio={friend.bio}
               image={friend.files}
               id={friend.id}
               addBuddy={() =>addBuddy(friend.id)}
@@ -104,42 +106,3 @@ const Addfriends = () => {
 
 export default Addfriends;
 
-
- // const responsive = {
-  //           desktop: {
-  //             breakpoint: { max: 3000, min: 1024 },
-  //             items: 3,
-  //             slidesToSlide: 3 // optional, default to 1.
-  //           },
-  //           tablet: {
-  //             breakpoint: { max: 1024, min: 464 },
-  //             items: 2,
-  //             slidesToSlide: 2 // optional, default to 1.
-  //           },
-  //           mobile: {
-  //             breakpoint: { max: 464, min: 0 },
-  //             items: 1,
-  //             slidesToSlide: 1 // optional, default to 1.
-  //           }
-  //         };
-  // return (
-  //     <> <LogoutBtn />
-  //       <Carousel
-  //         swipeable={false}
-  //         draggable={false}
-  //         showDots={true}
-  //        - responsive={responsive}
-  //         ssr={true} // means to render carousel on server-side.
-  //         infinite={true}
-  //         //   autoPlay={this.props.deviceType !== "mobile" ? true : false}
-  //         autoPlaySpeed={10000}
-  //         keyBoardControl={true}
-  //         customTransition="all .5"
-  //         transitionDuration={500}
-  //         containerClass="carousel-container"
-  //         //   removeArrowOnDeviceType={["tablet", "mobile"]}
-  //         //   deviceType={this.props.deviceType}
-  //         dotListClass="custom-dot-list-style"
-  //         itemClass="carousel-item-padding-40-px"
-  //       >
-          // {friends.map((friend, i) => 
