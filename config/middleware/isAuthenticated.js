944991a87d9d2147d03c49020736abbc7fc9c17f
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-30 12:42:04 -0700

    fixed authentication route

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -2,12 +2,5 @@
 module.exports = function (req, res, next) {
   console.log(`isauthenticated ${req.user}`)
   return req.user === null ? next(false) : next()
-  // if (req) {
-  //   // console.log(req.user)
-  //   // console.log(req.session.passport.user)
-  //   return next();
-  // }
-
-  // // If the user isn't logged in, redirect them to the login page
-  // return res.redirect("/login");
+ 
 };
