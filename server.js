6d6b9a8d77bc84e4d4c75e4b096b08ab3317c88d
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-31 12:12:02 -0700

    deployment

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("/", (req,res)=>{
-  res.sendFile(path.join(__dirname, 'build', "index.html"))
-})
+// app.get("/", (req,res)=>{
+//   res.sendFile(path.join(__dirname, 'build', "index.html"))
+// })
 
 const profileSeed = [{
 
